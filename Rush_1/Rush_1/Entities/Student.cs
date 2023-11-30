aae2f7563cdf9ebd53ff63ea513e9749c3e1fc9c
EVIL820 <150091877+EVIL820@users.noreply.github.com>
2023-11-30 17:03:30 -0600

    Add files via upload

---
+++ Rush_1/Rush_1/Entities/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace Rush_1.Entities
+{
+    public class Student
+    {
+        int StudentId { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }    
+        public char Gender { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
---
+++ Rush_1/Rush_1/Repositories/StudentRepository.cs
@@ -0,0 +1,45 @@
+﻿using Dapper;
+using Rush_1.Entities;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Rush_1.Repositories
+{
+    public class StudentRepository
+    {
+        private readonly IDbConnection _dbConnection;
+        public StudentRepository()
+        {
+            string connectionString = "Data Source=(local)\\SQLEXPRESS;Initial Catalog=School;User Id=sa;Password=3040;TrustServerCertificate=true;";
+            _dbConnection = new SqlConnection(connectionString);
+        }
+        public void InsertStudent(Student student )
+        {
+            try
+            {
+                string sql = "INSERT INTO Student (Name, Email, Gender, CreationDate) VALUES (@Name, @Email, @Gender, @CreationDate);";
+                _dbConnection.Execute(sql, student);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+        public Student GetStudentById(int studentId)
+        {
+            string sql = "SELECT * FROM Student WHERE StudentId = @StudentId;";
+            return _dbConnection.QuerySingleOrDefault<Student>(sql, new { StudentId = studentId });
+        }
+        public void UpdateStudent(Student student)
+        {
+            string sql = "UPDATE Student SET Name = @Name, Email = @Email, Gender = @Gender, CreationDate = @CreationDate WHERE StudentId = @StudentId;";
+            _dbConnection.Execute(sql, student);
+        }
+        public void DeleteStudent(int studentId)
+        {
+            string sql = "DELETE FROM Student WHERE StudentId = @StudentId;";
+            _dbConnection.Execute(sql, new { StudentId = studentId });
+        }
+    }
+}
